5ece1d35ea5f92f4c94154a0a9e1748c107d5cef
Saksham Sahgal <73836766+SakshamSahgal@users.noreply.github.com>
2022-08-19 01:51:34 +0530

    background Fixed and textbox working

--- JS/script.js
+++ JS/script.js
@@ -2,21 +2,23 @@ var width_slider = document.getElementById("width"); //getting the width slider
 var height_slider = document.getElementById("height"); //getting the height slider
 var canvas = document.getElementById("Output_Canvas"); //getting the canvas element
 var ctx = canvas.getContext('2d'); //getting 2d instance of canvas
-
+var car_lim = document.getElementById("char_count");
 redraw_canvas();
 
 width_slider.oninput = function() //function called when slider value changes
 {
-    console.log("width changed to " + width_slider.value);
+    //console.log("width changed to " + width_slider.value);
     canvas.width = width_slider.value;
     redraw_canvas();
+    update_UI();
 };
 
 height_slider.oninput = function() //function called when slider value changes
 {
-    console.log("width changed to " + height_slider.value);
+    //console.log("width changed to " + height_slider.value);
     canvas.height = height_slider.value;
     redraw_canvas();
+    update_UI();
 };
 
 function redraw_canvas()
@@ -34,3 +36,13 @@ function redraw_canvas()
     scannedImage.data = scannedData; //overiding the pixel data
     ctx.putImageData(scannedImage,0,0); //redrawing on canvas
 }
+
+function update_UI()
+{
+    car_lim.innerHTML = (canvas.width)*(canvas.height)*3;
+}
+
+input_textbox.oninput = function()
+{
+    console.log("likhakuch"); 
+};
